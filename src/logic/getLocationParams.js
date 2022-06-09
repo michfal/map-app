6ec43615bed46bab3ed557158d86841fb861419c
Michał Fal <michfal86@gmail.com>
2022-06-09 11:22:12 +0200

    get geocode

--- src/logic/getLocationParams.js
+++ src/logic/getLocationParams.js
@@ -15,5 +15,22 @@ export async function getLocationParams(adress) {
   console.log(data.items);
   // const position = await setPosition(data.items[0].position);
   const position = await data.items[0].position;
+  getRoute();
   return position;
+}
+
+async function getRoute() {
+  //   const searchAdress = 'Invalidenstr' + '117' + 'Berlin';
+
+  const response = await fetch(
+    // `&apiKey=${process.env.REACT_APP_HERE_API_KEY}`
+    `https://routematching.hereapi.com/2/calculateroute.json?routeMatch=1&mode=fastest;car;traffic:disabled&legal=access,oneway,thrutraf,turn&attributes=LINK_FCn(REF_NODE_NEIGHBOR_LINKS,NONREF_NODE_NEIGHBOR_LINKS)&file=undefined&apiKey=${process.env.REACT_APP_HERE_API_KEY}
+    `
+  );
+  const data = await response.json();
+
+  console.log(data);
+  // const position = await setPosition(data.items[0].position);
+  // const position = await data.items[0].position;
+  // return position;
 }
